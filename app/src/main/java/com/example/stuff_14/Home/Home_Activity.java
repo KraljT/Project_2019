c61388745452dc25deffc768709780032c2d0ed0
KraljT <kralj.t.str@gmail.com>
2019-06-01 19:42:51 +0200

    Added home screen, logout button, fixed showing 2 app icons

--- app/src/main/java/com/example/stuff_14/Home/Home_Activity.java
+++ app/src/main/java/com/example/stuff_14/Home/Home_Activity.java
@@ -1,15 +1,41 @@
 package com.example.stuff_14.Home;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.stuff_14.Login_Activity;
 import com.example.stuff_14.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class Home_Activity extends AppCompatActivity {
+public class Home_Activity extends AppCompatActivity implements View.OnClickListener{
+    private Button btn_logout;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btn_logout = findViewById(R.id.btn_Logout);
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() == null)
+        {
+            finish();
+            startActivity(new Intent(this, Login_Activity.class));
+        }
+        btn_logout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btn_logout)
+        {
+            mAuth.signOut();
+            finish();
+            startActivity(new Intent(this,Login_Activity.class));
+        }
     }
 }
