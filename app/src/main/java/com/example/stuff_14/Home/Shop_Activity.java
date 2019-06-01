1ae2318d8a4fed047f57010e6d38dd5810a1e2c6
KraljT <kralj.t.str@gmail.com>
2019-06-01 22:37:20 +0200

    added Shop button listners

--- app/src/main/java/com/example/stuff_14/Home/Shop_Activity.java
+++ app/src/main/java/com/example/stuff_14/Home/Shop_Activity.java
@@ -1,15 +1,59 @@
 package com.example.stuff_14.Home;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.stuff_14.R;
 
 public class Shop_Activity extends AppCompatActivity {
-
+    private Button btn_ach;
+    private Button btn_col;
+    private Button btn_home;
+    private Button btn_gps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+        btn_ach = findViewById(R.id.btn_achievement);
+        btn_col = findViewById(R.id.btn_collection);
+        btn_home = findViewById(R.id.btn_home);
+        btn_gps = findViewById(R.id.btn_gps);
+        //////////////////////////////////////////////////
+        btn_ach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Shop_Activity.this,Achievement_Activity.class);
+                startActivity(intent);
+            }
+        });
+        //////////////////////////////////////////////
+        btn_col.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Shop_Activity.this,Collection_Activity.class);
+                startActivity(intent);
+            }
+        });
+        //////////////////////////////////////////////
+        btn_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Shop_Activity.this,GPS_Activity.class);
+                startActivity(intent);
+            }
+        });
+        //////////////////////////////////////////////
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Shop_Activity.this,Home_Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
     }
 }
