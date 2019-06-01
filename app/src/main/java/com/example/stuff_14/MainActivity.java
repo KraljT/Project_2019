3f466fb817df36693fdf8c43045b4ff42ae46113
KraljT <kralj.t.str@gmail.com>
2019-06-01 21:51:38 +0200

    additional fixes

--- app/src/main/java/com/example/stuff_14/MainActivity.java
+++ app/src/main/java/com/example/stuff_14/MainActivity.java
@@ -1,6 +1,6 @@
-        package com.example.stuff_14;
+package com.example.stuff_14;
 
-import android.app.ProgressDialog;
+    import android.app.ProgressDialog;
         import android.content.Intent;
         import android.support.annotation.NonNull;
         import android.support.v7.app.AppCompatActivity;
