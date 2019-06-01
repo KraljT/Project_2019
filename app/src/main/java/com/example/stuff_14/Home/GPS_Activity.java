c070a72c70b7c318bf012cf85e6ec6610332021e
KraljT <kralj.t.str@gmail.com>
2019-06-01 22:15:17 +0200

    added 5 activits - Camera,GPS,Collection,Shop,Achievemnet

---
+++ app/src/main/java/com/example/stuff_14/Home/GPS_Activity.java
@@ -0,0 +1,15 @@
+package com.example.stuff_14.Home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.stuff_14.R;
+
+public class GPS_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps);
+    }
+}
