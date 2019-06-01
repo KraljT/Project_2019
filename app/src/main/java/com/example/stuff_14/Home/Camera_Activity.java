0b9d3c8f118c9d21c80ebb5bc60aaa12f4ea9d45
KraljT <kralj.t.str@gmail.com>
2019-06-01 22:32:57 +0200

    added Camera layout and java

--- app/src/main/java/com/example/stuff_14/Home/Camera_Activity.java
+++ app/src/main/java/com/example/stuff_14/Home/Camera_Activity.java
@@ -1,15 +1,43 @@
 package com.example.stuff_14.Home;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.stuff_14.R;
 
 public class Camera_Activity extends AppCompatActivity {
+    Button btn_camera;
+    ImageView camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        btn_camera = findViewById(R.id.btn_camera);
+        camera = findViewById(R.id.imageView_camera);
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile());
+                startActivityForResult(intent,1);
+
+            }
+        });
+
+
     }
-}
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data)
+    {
+        Bundle extras = data.getExtras();
+        super.onActivityResult(requestCode,resultCode,data);
+        Bitmap bitmap = (Bitmap)extras.get("data");
+        camera.setImageBitmap(bitmap);
+    }
